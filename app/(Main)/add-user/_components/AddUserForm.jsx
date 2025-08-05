bc47c9d1821c0499b2a5228cb5c328775eb6f523
kunda <kunda@ADMIN>
2025-08-05 12:36:18 +0530

    reset form added

--- app/(Main)/add-user/_components/AddUserForm.jsx
+++ app/(Main)/add-user/_components/AddUserForm.jsx
@@ -33,22 +33,23 @@ export default function AddUserForm() {
   function onSubmit(values) {
     try {
       console.log(values);
-      toast(
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
-        </pre>
-      );
     } catch (error) {
       console.error("Form submission error", error);
       toast.error("Failed to submit the form. Please try again.");
     }
   }
 
+  const { reset } = form;
+
+  const handleReset = () => {
+    reset({ firstName: "", lastName: "", email: "", phone: "" });
+  };
+
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-8 max-w-3xl mx-auto py-10"
+        className="space-y-8 max-w-3xl mx-auto py-10 shadow rounded-2xl p-3 bg-white"
       >
         <FormField
           control={form.control}
@@ -108,13 +109,17 @@ export default function AddUserForm() {
                   defaultCountry="IN"
                 />
               </FormControl>
-              <FormDescription>Enter your phone number.</FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
 
-        <Button type="submit">Submit</Button>
+        <div className="button space-x-3">
+          <Button type="reset" onClick={handleReset}>
+            Cancel
+          </Button>
+          <Button type="submit">Submit</Button>
+        </div>
       </form>
     </Form>
   );
